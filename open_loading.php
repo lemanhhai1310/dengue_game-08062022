89115a56c97af433cf235c0c2ac7ebef05cf7766
hailm <hai.lemanh.1310@gmail.com>
2022-06-11 11:05:34 +0700

    update

--- open_loading.php
+++ open_loading.php
@@ -19,6 +19,29 @@
         </div>
     </div>
 </div>
+<!-- Enter_game_alias -->
+<div id="modal-enter-game" class="uk-flex-top" uk-modal>
+    <div data-src="images/Board.png" uk-img class="opening__modal__body uk-modal-dialog uk-modal-body uk-margin-auto-vertical uk-flex uk-flex-bottom">
+        <div class="uk-width-1-1">
+            <div class="item__10">
+                <h2 class="uk-modal-title opening__modal__title">ENTER YOUR NAME</h2>
+            </div>
+            <div class="item__10">
+                <input class="uk-input opening__modal__input" type="text" placeholder="ENTER YOUR NAME">
+            </div>
+            <div class="item__10">
+                <div class="uk-child-width-auto uk-flex-center uk-grid-small" uk-grid>
+                    <div>
+                        <a href="" class="opening__modal__btn opening__modal__btn--cancel uk-button uk-button-default uk-modal-close"><span>CANCEL</span></a>
+                    </div>
+                    <div>
+                        <a href="" class="opening__modal__btn opening__modal__btn--submit uk-button uk-button-default"><span>SUBMIT</span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <script type="text/javascript">
     const imgData = [{
         name:'image1',
@@ -32,21 +55,22 @@
     }, {
         name:'image4',
         url:'https://picsum.photos/400/300/?random'
+    }, {
+        name:'image5',
+        url:'https://picsum.photos/900/300/?random'
+    }, {
+        name:'image6',
+        url:'https://picsum.photos/600/300/?random'
     }];
 
     const options = {
         resources: imgData, //resources expected a JSON or Array
         onStart: function(total) {
             console.log('onStart:' + total);
-            // document.querySelector(".status > span").innerHTML = 'onStart';
-            // $("body").addClass("uk-overflow-hidden");
             $('.opening__logo').hide();
         },
         onProgress: function(currentIndex, total) {
             console.log('onProgress:' + currentIndex + '/' + total);
-            // document.querySelector(".status > span").innerHTML = 'onProgress';
-            // document.querySelector(".division > span").innerHTML = currentIndex + '/' + total;
-            // document.querySelector(".percent > span").innerHTML = Math.floor((currentIndex / total) * 100) + '%';
             document.querySelector(".opening__progressbar__value").style.width = Math.floor((currentIndex / total) * 100) + '%';
             console.log(Math.floor((currentIndex / total) * 100));
         },
@@ -55,11 +79,11 @@
             console.log(result);
             $('.opening__progressbar').hide();
             $('.opening__logo').show();
-            // $("body").removeClass("uk-overflow-hidden");
-            // setTimeout(function () {
-            //     $(".loadpage").animate({ height: 0, opacity: 0.5 }, 'slow');
-            // },400);
-            // document.querySelector(".status > span").innerHTML = 'onComplete';
+            setTimeout(function () {
+                // code here
+                UIkit.modal('#modal-enter-game').show();
+                // console.log(1);
+            },6000);
         }
     };
 
